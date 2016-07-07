8e28358869b31e8c88861a3e9281c8a303436450
Sheun Aluko <alukosheun@gmail.com>
2016-07-07 03:45:24 -0700

    edited moreHelp

--- js/DSresources.js
+++ js/DSresources.js
@@ -6,7 +6,7 @@ File for defining resources that will be used by DS
 this.DSresources = {
     "affirmitive" : "OK" , 
     "help" : "Dream Saviour is a lightweight web application that will help you to record and keep track of your dreams. When you say new dream, I will guide you through entering the relevant information. When we are finished this information will be saved in a structured way for you to review later. Would you like to hear more?" , 
-    "moreHelp" : "Sorry there is no more help at this moment. Please check out the github repository for more information" , 
+    "moreHelp" : "Why dont you try say new dream then look at the information provided on the panel to the left. Please check out the github repository for more information. Take care" , 
     
 
 } 
